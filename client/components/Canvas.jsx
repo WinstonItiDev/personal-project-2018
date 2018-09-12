d68f6b5d553dbf5b6c237362ef212f30867043e8
WinstonItiDev <winstonshred@gmail.com>
2018-09-12 13:50:56 +1200

    added painting circles

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -6,29 +6,36 @@ class Canvas extends React.Component {
 
   constructor(props) {
     super(props)
-    console.log(props)
+    // console.log(props)
 
     this.state = {
-      x: 50,
-      y: 50
+      circles: []
     }
     this.clickReturnPos = this.clickReturnPos.bind(this)
   }
   
   clickReturnPos(e) {
-    console.log(e.clientX, e.clientY);
+
+    let newCircles = this.state.circles
+
+    newCircles.push({
+      cx: e.clientX,
+      cy: e.clientY,
+    })
+
     this.setState({
-      x: e.clientX - 55,
-      y: e.clientY
+      circles: newCircles,
     })
   }
 
   render() {
 
     return (
-
+      
         <svg className='svgCanvas' width={this.props.width} height={this.props.height} onClick={this.clickReturnPos}>
-          <Circle x={this.state.x} y={this.state.y}/>
+         {this.state.circles.map((circle, i)=> {
+           return <Circle key={'circle' + i}x={circle.cx} y={circle.cy}/>
+         })}
         </svg>
 
     )
